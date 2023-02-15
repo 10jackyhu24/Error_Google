8394e24da69dda4c7c89c55e9d70aa543b9b2761
10jackyhu24 <105954083+10jackyhu24@users.noreply.github.com>
2023-02-15 20:31:22 +0800

    2

--- javascript.js
+++ javascript.js
@@ -9,20 +9,33 @@ function google_application(){
     console.log(123);
 }
 
-var gmail_alert_text = ['確定要將網頁跳轉至Gmail?\n您可能不會得到任何回應，因為開發人員不想要coding了!',
-'當您看到這串文字，就代表您按下了Gmail。\n是不是相當驚喜呢?']
+
+// gmail -> alert
+var gmail_alert_text = ['林辰威，我要你的平板', '楊曜鳴薯條',  '胡晉瑋好醜', '蔡少華氣球', '胡傑棋超帥', '錢鋼教我數學', '譚翊軒帳號被盜']
 
 function gmail(){
     alert(gmail_alert_text[get_random_number(0, gmail_alert_text.length)])
 }
 
+
+// photo
+var photo_link = ['photo/1.jpg', 'photo/2.jpg', 'photo/3.jpg', 'photo/4.jpg', 'photo/5.jpg', 'photo/6.jpg', 'photo/7.jpg', 'photo/8.jpg', 'photo/9.jpg', 'photo/10.jpg', 'photo/11.jpg', 'photo/12.jpg', 'photo/13.jpg', 'photo/14.jpg', 'photo/15.jpg', 'photo/16.jpg', 'photo/17.jpg', 'photo/18.jpg', 'photo/19.jpg', 'photo/20.jpg', 'photo/21.jpg', 'photo/22.jpg', 'photo/23.jpg', 'photo/24.jpg', 'photo/25.jpg', 'photo/26.jpg', 'photo/27.jpg', 'photo/28.jpg', 'photo/29.jpg', 'photo/30.jpg'];
+
+function photo(){
+    document.body.style.background = `url(${photo_link[get_random_number(0, photo_link.length)]})`;
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundSize = 'contain';
+}
+
+
+// input bar -> play the mp4
 document.querySelector("#input_bar").addEventListener("keydown", (e)=>{
-    if(e.key == "Enter") doSomething();
+    if(e.key == "Enter") play_mp4();
 });
 
-function doSomething(){
-    // location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+function play_mp4(){
     document.getElementById("content").innerHTML = '<video src="./video/Rick_Astley.mp4" autoplay style="margin-top: 0px; height: 100vh;"></video>'
     document.getElementById("content").style.marginTop = '0px'
     document.getElementById("content").style.height = '90vh'
+    document.getElementById("content").style.backgroundSize = 'contain';
 }
